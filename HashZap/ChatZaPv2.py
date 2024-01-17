581a042e72b1558aead192ad3a4ad78e48df9698
Matheus-Allvz <workingaccont.matheus@gmail.com>
2024-01-17 14:05:20 -0300

    att

--- HashZap/ChatZaPv2.py
+++ HashZap/ChatZaPv2.py
@@ -1,16 +1,10 @@
 import flet as ft 
 
 def main(page):
-    # Itens da página
-    Title = ft.Text("ChatZaP", size=24)
-    user_name_text_camp = ft.TextField(label="Escreva seu nome aqui")
+    Title = ft.Text("ChatZaP", size=24, text_align=ft.CrossAxisAlignment.START)
 
-
-    campo_da_mensagem = ft.TextField(label="Digitar...")
-    botao_enviar_msg = ft.ElevatedButton("Enviar")
-    linha_de_mensagem = ft.Row([campo_da_mensagem, botao_enviar_msg])
-
-    chat = ft.Column()
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
 
     # DEF para ENTRAR no chat #
         # Ao entrar no chat eu preciso que:
@@ -24,32 +18,43 @@ def main(page):
         pop_up.open = False
         page.remove(Start_Button)
         page.add(chat)
+        chat.controls.append(ft.Text(f"{user_name_text_camp.value} entrou no ChatZaP."))
         page.add(linha_de_mensagem)
         page.update()
-    
+    user_name_text_camp = ft.TextField(label="Escreva seu nome aqui", on_submit=join_in_chat)
+
     # def para fechar o Pop-Up
     def close_popup(evento):
         page.dialog = pop_up
         pop_up.open = False
+        user_name_text_camp.value = ""
         page.update()
-
+    
+    # Configurações do POP-UP #
     pop_up = ft.AlertDialog(
     open=False,
     modal=True,
     title=ft.Text("Seja bem vindo ao ChatZaP"),
     content=user_name_text_camp,
-    actions=[ft.ElevatedButton("Entrar", on_click=join_in_chat), ft.ElevatedButton("Sair", on_click=close_popup)]
+    actions=[ft.ElevatedButton("Entrar", on_click=join_in_chat), ft.ElevatedButton("Sair", on_click=close_popup)],
     )
-        
-    def start_chat(evento):
+    # Evento de enviar mensagem #
+    def enviar_msg(evento):
+        msg_camp_text = f"{user_name_text_camp.value}: {campo_da_mensagem.value}"
+        chat.controls.append(ft.Text(msg_camp_text))
+        campo_da_mensagem.value = ""
+        page.update()
+
+    def start_chat_button(evento):
         page.dialog = pop_up
         pop_up.open = True
         page.update()
-        
-    Start_Button = ft.ElevatedButton(
-        "Start Chat",
-        on_click=start_chat
-        )
+
+    chat = ft.Column()
+    campo_da_mensagem = ft.TextField(label="Digitar...", on_submit=enviar_msg)
+    botao_enviar_msg = ft.ElevatedButton("Enviar", on_click=enviar_msg)
+    linha_de_mensagem = ft.Row([campo_da_mensagem, botao_enviar_msg], alignment=ft.MainAxisAlignment.CENTER)
+    Start_Button = ft.ElevatedButton("Start Chat", on_click=start_chat_button)
 
 
     page.add(Title)
--- HashZap/chat.py
+++ HashZap/chat.py
@@ -1,9 +1,13 @@
 import flet as ft 
 
 def main(page):
-    # Itens da página
     Title = ft.Text("ChatZaP", size=24)
 
+    def conect(info):
+        chat.controls.append(ft.Text(info))
+        page.update()
+
+    page.pubsub.subscribe(conect)
     # DEF para ENTRAR no chat #
         # Ao entrar no chat eu preciso que:
         # O pop-up se feche
@@ -16,51 +20,46 @@ def main(page):
         pop_up.open = False
         page.remove(Start_Button)
         page.add(chat)
-        chat.controls.append(ft.Text(f"{user_name_text_camp.value} entrou no ChatZaP."))
+        texto = f"{user_name_text_camp.value} entrou no ChatZaP."
+        page.pubsub.send_all(texto)
         page.add(linha_de_mensagem)
         page.update()
-    user_name_text_camp = ft.TextField(label="Escreva seu nome aqui", on_submit=join_in_chat)
-
+    user_name_text_camp = ft.TextField(label="Escreva seu nome aqui", on_submit=join_in_chat, max_length=16)
     # def para fechar o Pop-Up
     def close_popup(evento):
         page.dialog = pop_up
         pop_up.open = False
         user_name_text_camp.value = ""
         page.update()
-
+    
+    # Configurações do POP-UP #
     pop_up = ft.AlertDialog(
     open=False,
     modal=True,
     title=ft.Text("Seja bem vindo ao ChatZaP"),
     content=user_name_text_camp,
-    actions=[ft.ElevatedButton("Entrar", on_click=join_in_chat), ft.ElevatedButton("Sair", on_click=close_popup)]
+    actions=[ft.ElevatedButton("Entrar", on_click=join_in_chat), ft.ElevatedButton("Sair", on_click=close_popup)],
     )
-
+    # Evento de enviar mensagem #
     def enviar_msg(evento):
         msg_camp_text = f"{user_name_text_camp.value}: {campo_da_mensagem.value}"
-        chat.controls.append(ft.Text(msg_camp_text))
+        page.pubsub.send_all(msg_camp_text)
         campo_da_mensagem.value = ""
+        page.update()
+
+    def start_chat_button(evento):
+        page.dialog = pop_up
+        pop_up.open = True
         page.update()
 
     chat = ft.Column()
     campo_da_mensagem = ft.TextField(label="Digitar...", on_submit=enviar_msg)
+    Start_Button = ft.ElevatedButton("Start Chat", on_click=start_chat_button)
     botao_enviar_msg = ft.ElevatedButton("Enviar", on_click=enviar_msg)
     linha_de_mensagem = ft.Row([campo_da_mensagem, botao_enviar_msg])
-
-
-        
-    def start_chat(evento):
-        page.dialog = pop_up
-        pop_up.open = True
-        page.update()
-        
-    Start_Button = ft.ElevatedButton(
-        "Start Chat",
-        on_click=start_chat
-        )
 
 
     page.add(Title)
     page.add(Start_Button)
 
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER)
